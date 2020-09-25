8dd7c66f7d5c8977aab0ca2f5d39deac9bf7fb55
root <root@localhost.localdomain>
2020-09-26 01:33:41 +0800

    1st commit

---
+++ app/Http/Controllers/BmonitorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bmonitor;
+use Illuminate\Http\Request;
+
+class BmonitorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Bmonitor  $bmonitor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bmonitor $bmonitor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Bmonitor  $bmonitor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bmonitor $bmonitor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Bmonitor  $bmonitor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bmonitor $bmonitor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Bmonitor  $bmonitor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bmonitor $bmonitor)
+    {
+        //
+    }
+}
